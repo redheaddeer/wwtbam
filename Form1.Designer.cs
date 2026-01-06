9b5b43cf9a32cadff754079d845455c6acf1cbf0
Ксения Тапок <soulssfg@gmail.com>
2026-01-06 23:16:11 +0300

    beta 0.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,147 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(42, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Начало игры";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(45, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 33);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(45, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 28);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.CheckedChanged += new System.EventHandler(this.button1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(45, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(14, 13);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(455, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(14, 13);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(455, 350);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(14, 13);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(45, 420);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(800, 34);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Подтвердить выбор";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(45, 244);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(800, 30);
+            this.button0.TabIndex = 7;
+            this.button0.Text = "Выбор файла для начала игры";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(45, 420);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(800, 34);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Перейти к следующему вопросу";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Who Wants to Be a Millionaire?";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.RadioButton button1;
+        private System.Windows.Forms.RadioButton button2;
+        private System.Windows.Forms.RadioButton button3;
+        private System.Windows.Forms.RadioButton button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,229 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace wwtbam
 {
     public partial class Form1 : Form
     {
+        private string[] gameData = null;
+        private TaskCompletionSource<bool> nextStep;
+
+        private string right = "";
+        private string selected = "";
+
         public Form1()
         {
             InitializeComponent();
+            Console.Out.Write("\nНачало игры\n");
+            label1.Text = "Начало игры";
+            textBox1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button0.Visible = true;
+            button0.Text = "Выберите файл для начала игры";
+        }
+
+        private async Task Rounds(string[] gameData) {
+            foreach (var roundData in gameData)
+            {
+                nextStep = new TaskCompletionSource<bool>();
+                string[] data = roundData.Split(';');
+                string round = data[0];
+
+                string question = data[1];
+                string _a = data[2];
+                string _b = data[3];
+                string _c = data[4];
+                string _d = data[5];
+                right = data[6];
+                selected = "";
+
+                label1.Text = "Раунд " + round;
+                textBox1.Visible = true;
+                textBox1.Text = question;
+
+                resetState();
+
+                button1.Text = _a;
+                button2.Text = _b;
+                button3.Text = _c;
+                button4.Text = _d;
+
+
+                while (!button5.Enabled)
+                {
+                    if (button1.Checked)
+                    {
+                        button5.Enabled = true;
+                        selected = "A";
+                    } else if (button2.Checked)
+                    {
+                        button5.Enabled = true;
+                        selected = "B";
+                    }
+                    else if (button3.Checked)
+                    {
+                        button5.Enabled = true;
+                        selected = "C";
+                    }
+                    else if ( button4.Checked)
+                    {
+                        button5.Enabled = true;
+                        selected = "D";
+                    }
+                    await Task.Delay(100);
+                }
+
+                while (!button6.Visible)
+                {
+                    await Task.Delay(100);
+                }
+                await nextStep.Task;
+            }
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV файлы (*.csv)|*.csv|Все файлы (*.*)|*.*",
+                Title = "Выберите файл CSV"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Читаем строки из выбранного файла
+                    gameData = File.ReadAllLines(openFileDialog.FileName, Encoding.UTF8);
+
+                    MessageBox.Show("Файл успешно загружен!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка чтения файла: {ex.Message}");
+                }
+            }
+            Rounds(gameData);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            showRight(right, selected);
+            button5.Visible = false;
+            button6.Visible = true;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button1.Checked = false;
+            button2.Checked = false;
+            button3.Checked = false;
+            button4.Checked = false;
+            button5.Visible = false;
+            button6.Visible = true;
+            nextStep.TrySetResult(true);
+        }
+
+        private void showRight(string right, string selected) {
+            string fixedRight = "";
+            switch (right)
+            {
+                case "A":
+                case "А":
+                    fixedRight = "A";
+                    break;
+                case "B":
+                case "Б":
+                    fixedRight = "B";
+                    break;
+                case "C":
+                case "В":
+                    fixedRight = "C";
+                    break;
+                case "D":
+                case "Г":
+                    fixedRight = "D";
+                    break;
+                default:
+                    fixedRight = right;
+                    break;
+            }
+            switch (fixedRight)
+            {
+                case "A":
+                    button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    button1.ForeColor = System.Drawing.Color.Green;
+                    break;
+                case "B":
+                    button2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    button2.ForeColor = System.Drawing.Color.Green;
+                    break;
+                case "C":
+                    button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    button3.ForeColor = System.Drawing.Color.Green;
+                    break;
+                case "D":
+                    button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    button4.ForeColor = System.Drawing.Color.Green;
+                    break;
+                default:
+                    break;
+            }
+            if (fixedRight != selected) {
+                switch (selected)
+                {
+                    case "A":
+                        button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                        button1.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    case "B":
+                        button2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                        button2.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    case "C":
+                        button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                        button3.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    case "D":
+                        button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                        button4.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void resetState() {
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = true;
+
+            button1.Checked = false;
+            button2.Checked = false;
+            button3.Checked = false;
+            button4.Checked = false;
+
+            button5.Visible = true;
+            button5.Enabled = false;
+            button5.Visible = true;
+            button6.Visible = true;
+            button0.Visible = false;
+
+            button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            button1.ForeColor = System.Drawing.Color.Black;
+            button2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            button2.ForeColor = System.Drawing.Color.Black;
+            button3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            button3.ForeColor = System.Drawing.Color.Black;
+            button4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            button4.ForeColor = System.Drawing.Color.Black;
         }
     }
 }
